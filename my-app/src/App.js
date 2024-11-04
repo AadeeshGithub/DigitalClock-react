d09d4bd7cda50d223699a8ff6dfcca3f1d876a90
Aadeesh <aadeesh1501@chitkara.edu.in>
2024-11-04 15:02:52 +0530

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DigitalClock from './design';
+
+function App() {
+    return (
+        <div>
+            <DigitalClock />
+        </div>
+    );
+}
+
+export default App;
